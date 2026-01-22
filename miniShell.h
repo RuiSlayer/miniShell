fcef8705f5195a84cec9c6ffad2eedc33e53a564
Rui Costa <ruiafonsocosta@gmail.com>
2026-01-22 19:53:26 +0000

    signal handle done ctrl + c , ctrl + d , ctrol + \

--- miniShell.h
+++ miniShell.h
@@ -6,10 +6,11 @@
 /*   By: slayer <slayer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 18:58:38 by slayer            #+#    #+#             */
-/*   Updated: 2026/01/20 22:27:58 by slayer           ###   ########.fr       */
+/*   Updated: 2026/01/22 17:52:58 by slayer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
