04fbbd86296f67d84b80caf5e5d0024796f8e97e
Francisco Gameiro <fgameiro@c1r2s6.42lisboa.com>
2026-02-24 21:13:54 +0000

    Makefile changes and some changes in tokens_utils.c

--- tokens/tokens_utils.c
+++ tokens/tokens_utils.c
@@ -19,18 +19,6 @@ int	ft_is_quote(char c)
 	return (0);
 }
 
-int	ft_is_separator(char *s)
-{
-	return (*s == ' ' || *s == '\t' || *s == '<' || *s == '>' || *s == '|');
-}
-
-int ft_isspace(int c)
-{
-    return (c == ' '  || c == '\t' ||
-            c == '\n' || c == '\v' ||
-            c == '\f' || c == '\r');
-}
-
 void	ft_skip_spaces(char **line)
 {
 	while (**line && ft_isspace(**line))
@@ -48,24 +36,6 @@ char *ft_strchr(const char *s, int c)
     if ((char)c == '\0')
         return ((char *)s);
     return (NULL);
-}
-
-int ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-    size_t i;
-
-    if (n == 0)
-        return (0);
-    i = 0;
-    while (i < n && s1[i] && s2[i])
-    {
-        if ((unsigned char)s1[i] != (unsigned char)s2[i])
-            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-        i++;
-    }
-    if (i < n)
-        return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-    return (0);
 }
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
@@ -93,65 +63,6 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
     sub[i] = '\0';
     return (sub);
 }
-
-char *ft_strdup(const char *s)
-{
-    char    *dup;
-    size_t  len;
-    size_t  i;
-
-    len = ft_strlen(s);
-    dup = (char *)malloc(len + 1);
-    if (!dup)
-        return (NULL);
-    i = 0;
-    while (i < len)
-    {
-        dup[i] = s[i];
-        i++;
-    }
-    dup[i] = '\0';
-    return (dup);
-}
-
-size_t ft_strlen(const char *s)
-{
-    size_t i = 0;
-    while (s[i])
-        i++;
-    return (i);
-}
-
-void *ft_calloc(size_t count, size_t size)
-{
-    void    *ptr;
-    size_t  total;
-
-    if (size != 0 && count > SIZE_MAX / size)
-        return (NULL);  // overflow protection
-
-    total = count * size;
-    ptr = malloc(total);
-    if (!ptr)
-        return (NULL);
-
-    ft_bzero(ptr, total);
-    return (ptr);
-}
-
-void ft_bzero(void *s, size_t n)
-{
-    unsigned char *p;
-
-    p = (unsigned char *)s;
-    while (n > 0)
-    {
-        *p = 0;
-        p++;
-        n--;
-    }
-}
-
 
 bool	ft_skip_quotes(char *line, size_t *i)
 {
