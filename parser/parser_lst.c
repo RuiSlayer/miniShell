0d6a8b6ac323ec11a83bfbc567ae7595165db377
Francisco Gameiro <fgameiro@c1r2s6.42lisboa.com>
2026-02-25 01:45:09 +0000

    parsing added

---
+++ parser/parser_lst.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_lst.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgameiro <fgameiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/25 00:35:32 by fgameiro          #+#    #+#             */
+/*   Updated: 2026/02/25 00:53:56 by fgameiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+static int  ft_add_arg(t_cmd *cmd, char *value)
+{
+    int     count;
+    char    **new_args;
+
+    count = 0;
+    while (cmd->args && cmd->args[count])
+        count++;
+    new_args = ft_realloc(cmd->args, sizeof(char *) * (count + 2));
+    if (!new_args)
+        return (0);
+    new_args[count] = ft_strdup(value);
+    new_args[count + 1] = NULL;
+    cmd->args = new_args;
+    return (1);
+}
+
+static int  ft_add_redir(t_cmd *cmd, t_redir_type type, char *file)
+{
+    t_redir *new;
+    t_redir *curr;
+
+    new = ft_calloc(1, sizeof(t_redir));
+    if (!new)
+        return (0);
+    
+}
