bd00f353acb441d218c695da23029cf9a358ca37
Francisco Gameiro <fgameiro@c1r8s4.42lisboa.com>
2026-03-18 20:24:12 +0000

    Fixed leaks in parsing and tested the changes in a few places, created a different folder for expansion

--- incs/expansion.h
+++ incs/expansion.h
@@ -1,12 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser_expand.c                                    :+:      :+:    :+:   */
+/*   expansion.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgameiro <fgameiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/12 16:42:17 by fgameiro          #+#    #+#             */
-/*   Updated: 2026/03/12 16:42:19 by fgameiro         ###   ########.fr       */
+/*   Created: 2026/03/18 19:50:50 by fgameiro          #+#    #+#             */
+/*   Updated: 2026/03/18 19:52:15 by fgameiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EXPANSION_H
+# define EXPANSION_H
+# include "miniShell_exec.h"
+
+
+
+
+
+#endif
