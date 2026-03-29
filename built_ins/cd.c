e62d825f4b508ad7f0e5368395d7d34ac8554595
Rui Costa <rucosta@c1r1s11.42lisboa.com>
2026-03-29 00:49:48 +0000

    bug fixes on cd

--- built_ins/cd.c
+++ built_ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: rucosta <rucosta@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 17:55:19 by slayer            #+#    #+#             */
-/*   Updated: 2026/03/28 21:19:06 by rucosta          ###   ########.fr       */
+/*   Updated: 2026/03/29 00:42:47 by rucosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,24 +37,41 @@ int	update_pwd(char *old_pwd, t_env **env)
 	return (0);
 }
 
+int	change_dir(char *dir, char	*old_pwd, t_env **env)
+{
+	if (chdir(dir) != 0)
+			return (perror("cd"), free(old_pwd), 1);
+		return (update_pwd(old_pwd, env), 0);
+}
+
+int	chage_by_var(char *old_pwd, t_env **env, char *name)
+{
+	char	*val;
+
+	val = ft_getenv(*env, name);
+		if (!val || ft_strcmp(val, "") == 0)
+			return (printf("minishell: cd: %s not set\n", name), free(old_pwd), 1);
+		return (change_dir(val, old_pwd, env), 0);
+}
+
 int	cd(t_cmd *cmds, t_env **env)
 {
 	char	*old_pwd;
-	char	*home;
 
 	old_pwd = getcwd(NULL, 0);
-	if (!old_pwd)
-		return (perror("minishell: cd: OLDPWD not set\n"), 1);
+	if (!old_pwd || ft_strcmp(old_pwd, "") == 0)
+		return (perror("minishell: cd: PWD not set\n"), 1);
 	if(!cmds->args[1])
+		return (chage_by_var(old_pwd, env, "HOME"));
+	if (ft_strcmp(cmds->args[1], "--") == 0 && cmds->args[2])
+		return (change_dir(cmds->args[2], old_pwd, env));
+	if(cmds->args[2])
+		return (printf("minishell: cd: too many arguments\n"), free(old_pwd), 1);
+	if (ft_strcmp(cmds->args[1], "-") == 0)
 	{
-		home = ft_getenv(*env, "HOME");
-		if(!home || home[0])
-			return(printf("minishell: cd: HOME not set\n"), free(old_pwd), 1);
-		if (chdir(home) != 0)
-			return (perror("cd"), free(old_pwd), 1);
-		return (update_pwd(old_pwd, env), 0);
+		if(chage_by_var(old_pwd, env, "OLDPWD"))
+			return (1);
+		return(printf("%s\n", ft_getenv(*env, "OLDPWD")));
 	}
-	if (chdir(cmds->args[1]) != 0)
-		return (perror("cd"), free(old_pwd), 1);
-	return (update_pwd(old_pwd, env), 0);
+	return (change_dir(cmds->args[1], old_pwd, env));
 }
