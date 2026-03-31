fab2b73d86248169f42c054cd0b76d48aab085b3
Rui Costa <ruiafonsocosta@gmail.com>
2026-03-31 04:13:24 +0100

    external programs added need to create a funtion handle and search the PATH variable

--- main_exec.c
+++ main_exec.c
@@ -3,32 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   main_exec.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgameiro <fgameiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: slayer <slayer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 18:58:13 by slayer            #+#    #+#             */
-/*   Updated: 2026/03/28 20:23:24 by fgameiro         ###   ########.fr       */
+/*   Updated: 2026/03/31 03:52:31 by slayer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "incs/miniShell_exec.h"
 
+char	*resolve_path(char *cmd, char **envp)
+{
+    // If cmd is already absolute or relative
+    if (cmd[0] == '/' || cmd[0] == '.')
+        return cmd;
+
+    // Find PATH in envp
+    char *path_env = NULL;
+    for (int i = 0; envp[i]; i++) {
+        if (ft_strncmp(envp[i], "PATH=", 5) == 0) {
+            path_env = envp[i] + 5;
+            break;
+        }
+    }
+    if (!path_env) return NULL;
+
+    // Try each directory in PATH
+    char *dirs = ft_strdup(path_env);
+    char *dir  = strtok(dirs, ":"); // need to create a auxiliar function
+    while (dir) {
+        char full[1024];
+        snprintf(full, sizeof(full), "%s/%s", dir, cmd);
+        if (access(full, X_OK) == 0)
+            return strdup(full);
+        dir = strtok(NULL, ":");
+    }
+    free(dirs);
+    return NULL; // not found
+}
+
+int	external_cmds(t_shell *shell)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid < 0)
+		return (perror("fork"), 1);
+	if (pid == 0)
+	{
+		// Child process
+		char **envp = env_to_array(shell->env);
+		char *path = resolve_path(shell->cmds->args[0], envp); // find binary in PATH
+		if (!path || execve(path, shell->cmds->args, envp) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		// Parent waits
+		int status;
+		waitpid(pid, &status, 0);
+	}
+	return (0);
+}
+
 int	cmd_eval(t_shell *shell)
 {
-
 	if (ft_strcmp(shell->cmds->args[0], "exit") == 0)
 		return (1);
-	if (ft_strcmp(shell->cmds->args[0], "echo") == 0)
+	else if (ft_strcmp(shell->cmds->args[0], "echo") == 0)
 		echo(shell->cmds);
-	if (ft_strcmp(shell->cmds->args[0], "pwd") == 0)
+	else if (ft_strcmp(shell->cmds->args[0], "pwd") == 0)
 		pwd();
-	if (ft_strcmp(shell->cmds->args[0], "env") == 0)
+	else if (ft_strcmp(shell->cmds->args[0], "env") == 0)
 		print_env(shell->env);
-	if (ft_strcmp(shell->cmds->args[0], "export") == 0)
+	else if (ft_strcmp(shell->cmds->args[0], "export") == 0)
 		built_export(shell->cmds, &shell->env);
-	if (ft_strcmp(shell->cmds->args[0], "cd") == 0)
+	else if (ft_strcmp(shell->cmds->args[0], "cd") == 0)
 		cd(shell->cmds, &shell->env);
-	if (ft_strcmp(shell->cmds->args[0], "unset") == 0)
+	else if (ft_strcmp(shell->cmds->args[0], "unset") == 0)
 		unset(shell->cmds, &shell->env);
+	else
+		external_cmds(shell);
 	return (0);
 }
 
