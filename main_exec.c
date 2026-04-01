a9d3f1a5d14cd90a836a9180debc432076e0e3d9
Rui Costa <rucosta@c1r1s11.42lisboa.com>
2026-04-01 22:26:30 +0100

    refator of pipe_loop and cmds_runers

--- main_exec.c
+++ main_exec.c
@@ -3,65 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main_exec.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slayer <slayer@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rucosta <rucosta@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 18:58:13 by slayer            #+#    #+#             */
-/*   Updated: 2026/04/01 18:59:08 by slayer           ###   ########.fr       */
+/*   Updated: 2026/04/01 22:25:44 by rucosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "incs/miniShell_exec.h"
-
-int	external_cmds(t_shell *shell)
-{
-	char **envp = env_to_array(shell->env);
-	char *path = ft_find_path(shell->cmds->args[0], shell->env); // find binary in PATH
-	
-	if (!path || execve(path, shell->cmds->args, envp) == -1)
-	{
-		perror("execve");
-		exit(EXIT_FAILURE);
-	}
-	return (0);
-}
-
-int	run_builtin(t_shell *shell)
-{
-	if (ft_strcmp(shell->cmds->args[0], "exit") == 0)
-		return (1);
-	else if (ft_strcmp(shell->cmds->args[0], "echo") == 0)
-		return (echo(shell->cmds));
-	else if (ft_strcmp(shell->cmds->args[0], "pwd") == 0)
-		return (pwd());
-	else if (ft_strcmp(shell->cmds->args[0], "env") == 0)
-		return (print_env(shell->env));
-	else if (ft_strcmp(shell->cmds->args[0], "export") == 0)
-		return (built_export(shell->cmds, &shell->env));
-	else if (ft_strcmp(shell->cmds->args[0], "cd") == 0)
-		return (cd(shell->cmds, &shell->env));
-	else if (ft_strcmp(shell->cmds->args[0], "unset") == 0)
-		return (unset(shell->cmds, &shell->env));
-	return (0);
-}
-
-int	is_builtin(t_shell *shell)
-{
-	if (ft_strcmp(shell->cmds->args[0], "exit") == 0)
-		return (1);
-	else if (ft_strcmp(shell->cmds->args[0], "echo") == 0)
-		return (1);
-	else if (ft_strcmp(shell->cmds->args[0], "pwd") == 0)
-		return (1);
-	else if (ft_strcmp(shell->cmds->args[0], "env") == 0)
-		return (1);
-	else if (ft_strcmp(shell->cmds->args[0], "export") == 0)
-		return (1);
-	else if (ft_strcmp(shell->cmds->args[0], "cd") == 0)
-		return (1);
-	else if (ft_strcmp(shell->cmds->args[0], "unset") == 0)
-		return (1);
-	return (0);
-}
 
 int	apply_redirects(t_redir *redir)
 {
@@ -100,101 +49,6 @@ int	apply_redirects(t_redir *redir)
 	return 0;
 }
 
-void	run_builtin_in_parent(t_cmd *cmd, t_shell *shell)
-{
-	int saved_in  = dup(STDIN_FILENO);
-	int saved_out = dup(STDOUT_FILENO);
-
-	// Aplica redirects temporariamente no pai
-	if (apply_redirects(cmd->redirs) == -1)
-	{
-		dup2(saved_in,  STDIN_FILENO);
-		dup2(saved_out, STDOUT_FILENO);
-		close(saved_in); close(saved_out);
-		return ;
-	}
-
-	shell->exit_status = run_builtin(shell);
-
-	// Restaura stdin/stdout originais
-	dup2(saved_in,  STDIN_FILENO);
-	dup2(saved_out, STDOUT_FILENO);
-	close(saved_in);
-	close(saved_out);
-}
-
-void	child_process(t_cmd *cmd, int prev_fd, int pipe_fd[], t_shell *shell)
-{
-    // 1. Conecta ao pipe anterior (stdin)
-    if (prev_fd != -1)
-    {
-        dup2(prev_fd, STDIN_FILENO);
-        close(prev_fd);
-    }
-    // 2. Conecta ao pipe seguinte (stdout)
-    if (cmd->next)
-    {
-        dup2(pipe_fd[1], STDOUT_FILENO);
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
-    }
-    // 3. Aplica redirects — em ordem, sobrescrevem os pipes se necessário
-	if (apply_redirects(cmd->redirs) == -1)
-		exit(1);
-	shell->cmds = cmd;
-	if (is_builtin(shell))
-		exit(run_builtin(shell)); // builtin no filho quando há pipe
-	external_cmds(shell);
-	perror(cmd->args[0]);
-	exit(126);
-}
-
-void	execute_pipeline(t_shell *shell)
-{
-	int     pipe_fd[2];
-	int     prev_fd = -1;   // read end do pipe anterior
-	t_cmd   *cmd = shell->cmds;
-	pid_t   last_pid;
-
-	// Caso especial: comando único builtin sem pipe
-	if (!cmd->next && is_builtin(shell))
-	{
-		run_builtin_in_parent(cmd, shell);
-		return ;
-	}
-	while (cmd)
-	{
-		// Cria pipe se ainda há comandos a seguir
-		if (cmd->next && pipe(pipe_fd) == -1)
-		{
-			// exit_error("pipe");
-			return;
-		}
-		last_pid = fork();
-		if (last_pid == -1)
-		{
-			// exit_error("fork");
-			return;
-		}
-		if (last_pid == 0)
-			child_process(cmd, prev_fd, pipe_fd, shell);
-
-		// Pai fecha os fds que passou ao filho
-		if (prev_fd != -1)
-			close(prev_fd);
-		if (cmd->next)
-		{
-			close(pipe_fd[1]);       // pai nunca escreve
-			prev_fd = pipe_fd[0];    // guarda read end para próximo filho
-		}
-		cmd = cmd->next;
-	}
-	// Pai espera por todos os filhos
-	waitpid(last_pid, &shell->exit_status, 0);
-	while (wait(NULL) > 0)
-		;
-}
-
 int	main(int argc, char **argv, char **envp)
 {
 	t_shell		shell;
@@ -212,10 +66,7 @@ int	main(int argc, char **argv, char **envp)
 	{
 		line = readline("prompt> ");
 		if (line == NULL)
-		{
-			write(STDOUT_FILENO, "\n", 1);
-			return (rl_clear_history(), free_env(shell.env), 0);
-		}
+			return (write(STDOUT_FILENO, "\n", 1), rl_clear_history(), free_env(shell.env), 0);
 		if (*line == '\0')
 		{
 			free(line);
@@ -231,8 +82,6 @@ int	main(int argc, char **argv, char **envp)
 		if (!shell.cmds)
 			continue ;
 		ft_expand(&shell);
-		/* if (cmd_eval(&shell))
-			return (rl_clear_history(), free_env(shell.env), ft_free_cmd_list(&shell.cmds), shell.exit_status); */
 		execute_pipeline(&shell);
 			/* return (rl_clear_history(), free_env(shell.env), ft_free_cmd_list(&shell.cmds), shell.exit_status); */
 		ft_free_cmd_list(&shell.cmds);
